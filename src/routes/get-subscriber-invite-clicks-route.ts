4a9010a77195641cfd7f61e282cf938324994ead
ezin1 <eziof.dev@gmail.com>
2025-02-22 10:01:11 -0300

    feat: add ranking functionality with new route and database integration, update build process, and modify .gitignore

--- src/routes/get-subscriber-invite-clicks-route.ts
+++ src/routes/get-subscriber-invite-clicks-route.ts
@@ -1,7 +1,5 @@
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
 import z from 'zod'
-import { env } from '../env'
-import { accessInviteLink } from '../functions/access-invite-link'
 import { getSubscriberInviteClicks } from '../functions/get-subscriber-invite-clicks'
 
 export const getSubscriberInviteClicksRoute: FastifyPluginAsyncZod =
--- src/server.ts
+++ src/server.ts
@@ -15,6 +15,7 @@ import { subscribeToEventRoute } from './routes/subscribe-to-event-route'
 import { getSubscriberInviteClicksRoute } from './routes/get-subscriber-invite-clicks-route'
 import { getSubscriberInvitesCountRoute } from './routes/get-subscriber-invites-count-route'
 import { getSubscriberRankingPositionRoute } from './routes/get-subscriber-ranking-position-route'
+import { getRankingRoute } from './routes/get-ranking-route'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -44,6 +45,7 @@ app.register(accessInviteLinkRoute)
 app.register(getSubscriberInviteClicksRoute)
 app.register(getSubscriberInvitesCountRoute)
 app.register(getSubscriberRankingPositionRoute)
+app.register(getRankingRoute)
 
 app.listen({ port: env.PORT }).then(() => {
   console.log('HTTP server running!')
